b555465ecfeb08a49a3ef58886e04d436d702761
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2024-05-23 09:02:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionComponent from './components/FunctionComponent';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <FunctionComponent/>
         </p>
         <a
           className="App-link"
---
+++ src/components/FunctionComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+const FunctionComponent = () => {
+    return (
+        <>
+            <h3>Component 1</h3>
+        </>
+    );
+}
+export default FunctionComponent;
